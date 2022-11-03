683a9abf046f988cfe7aa8341f188dc047693bd7
Aviram Fishman <fishmanaviram@gmail.com>
2022-11-03 13:02:38 +0200

    dsf

--- ThreadCheckArray/src/SharedData.java
+++ ThreadCheckArray/src/SharedData.java
@@ -3,7 +3,7 @@ public class SharedData
 	private int [] array;
 	private boolean [] winArray;
 	private boolean flag;
-	private final int b;//kjgjhghjjkjjhggfb
+	private final int b;//kjgjhghjjkjjhggfbhjg
 	
 	public SharedData(int[] array, int b) {
 		
